faab12a1d90c5725d932bc3d6ef01e2ca3dffc71
Zaghar <haqspeed@gmail.com>
2020-09-21 17:14:51 +0100

    Update add-domain.sh

--- add-domain.sh
+++ add-domain.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 echo "please put domain name:"
 read domain
-sudo mkdir -p "/var/www/$domain/public_html"
-sudo chmod 755 "/var/www/$domain/public_html"
-echo "please put proxy ip:port\nex:127.0.0.1:3000\n"
+mkdir -p "/var/www/$domain/public_html"
+chmod 755 "/var/www/$domain/public_html"
+echo "please put proxy ip:port
+ex:127.0.0.1:3000"
 read proxy
 echo "server {
 	listen 80;
@@ -13,13 +14,13 @@ echo "server {
    location / {
 		proxy_pass http://$proxy;
                 proxy_http_version 1.1;
-                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Upgrade \$http_upgrade;
                 proxy_set_header Connection 'upgrade';
-                proxy_set_header Host $host;
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_set_header X-Fowarded-For $proxy_add_x_forwarded_for;
-                proxy_set_header X-Fowarded-Proto $scheme;
-                proxy_cache_bypass $http_upgrade;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Fowarded-For \$proxy_add_x_forwarded_for;
+                proxy_set_header X-Fowarded-Proto \$scheme;
+                proxy_cache_bypass \$http_upgrade;
    }
    error_page 500 502 503 504 /50x.html;
    location = /50x.html {
@@ -35,5 +36,5 @@ server {
 		autoindex off;
 	}
 }">"/etc/nginx/sites-available/$domain.conf"
-sudo ln -s "/etc/nginx/sites-available/$domain.conf" "/etc/nginx/sites-enabled/$domain.conf"
+ln -s "/etc/nginx/sites-available/$domain.conf" "/etc/nginx/sites-enabled/$domain.conf"
 echo "now you can restart your nginx server"
