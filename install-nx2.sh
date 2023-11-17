77b766f1d2bc38c6b1031850accdf6f2a6bb4859
ixover <48690643+ixover@users.noreply.github.com>
2023-11-17 08:55:10 +0100

    Update install-nx2.sh

--- install-nx2.sh
+++ install-nx2.sh
@@ -103,7 +103,7 @@ mkdir /etc/nginx/sites-enabled
 sudo systemctl enable nginx
 sudo service nginx restart
 echo "installing nodejs ..."
-curl -sL https://rpm.nodesource.com/setup_19.x | sudo bash -
+curl -sL https://rpm.nodesource.com/setup_20.x | sudo bash -
 sudo yum -y install nodejs
 sudo yum -y install gcc-c++ make
 sudo yum -y install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc
